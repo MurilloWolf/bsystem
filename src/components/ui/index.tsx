50c588ea5047fc9301063bdc0e7ce1ac0d32e7be
MurilloWolf <mwolfcavalheiro@gmail.com>
2025-01-07 19:28:08 -0300

    feat: added shadcn ui components

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,43 @@
+import { Button, buttonVariants } from "./button";
+import { Label } from "./label";
+import { Input } from "./input";
+import { Separator } from "@radix-ui/react-separator";
+import {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "./form";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./card";
+export {
+  Separator,
+  Input,
+  Label,
+  Button,
+  buttonVariants,
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+};
